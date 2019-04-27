6c43fef2f97d6ddfc1ff49add80e5394d6ccfe7e
mskz-3110 <mskz.saito@gmail.com>
2019-04-27 18:17:09 +0900

    appveyor

--- build_appveyor.sh
+++ build_appveyor.sh
@@ -8,14 +8,14 @@ shift
 
 echo "APPVEYOR_BUILD_WORKER_IMAGE=${APPVEYOR_BUILD_WORKER_IMAGE} platform=${platform} configuration=${configuration}"
 case "${APPVEYOR_BUILD_WORKER_IMAGE}" in
-Visual Studio 2013)
-  
+"Visual Studio 2013")
+  echo 2012 2013
   ;;
-Visual Studio 2015)
-  
+"Visual Studio 2015")
+  echo 2015
   ;;
-Visual Studio 2017)
-  
+"Visual Studio 2017")
+  echo 2017
   ;;
 esac
 
