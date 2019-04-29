80965d4570967148fdb16ea3433d9bb41193c67a
mskz-3110 <mskz.saito@gmail.com>
2019-04-30 08:39:17 +0900

    android

--- install_packages.sh
+++ install_packages.sh
@@ -6,7 +6,7 @@ if [ -f /etc/redhat-release ]; then
   yum install -y bzip2 openssl-devel readline-devel zlib-devel
 elif [ -f /etc/issue ]; then
   apt update
-  apt install -y wget
+  apt install -y wget unzip file
   apt install -y git gcc g++ cmake make
   apt install -y bzip2 libssl-dev libreadline-dev zlib1g-dev
 fi
