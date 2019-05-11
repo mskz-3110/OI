9774c3d08c765147077beb79a87d49274747cb27
mskz-3110 <mskz.saito@gmail.com>
2019-05-11 18:49:46 +0900

    update

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-if [ -d ~/.rbenv ]; then
-  export PATH="$HOME/.rbenv/bin:$PATH"
-  eval "$(rbenv init -)"
-elif [ -n "${RUBY_VERSION}" ]; then
+if [ -n "${RUBY_VERSION}" ]; then
   if [ ! -d ~/.rbenv ]; then
     git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+  else
+    pushd ~/.rbenv
+      git pull origin master
+    popd
   fi
   
   export PATH="$HOME/.rbenv/bin:$PATH"
   
   if [ ! -d  ~/.rbenv/plugins/ruby-build ]; then
     git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+  else
+    pushd ~/.rbenv/plugins/ruby-build
+      git pull origin master
+    popd
   fi
   
   eval "$(rbenv init -)"
   
   RUBY_BUILD_CACHE_PATH=/tmp CONFIGURE_OPTS="--disable-install-rdoc" rbenv install ${RUBY_VERSION}
   rbenv global ${RUBY_VERSION}
+elif [ -d ~/.rbenv ]; then
+  export PATH="$HOME/.rbenv/bin:$PATH"
+  eval "$(rbenv init -)"
 fi
